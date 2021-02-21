43f56ceb3609c148c339281f7d3ae34e1188e205
prabhunath111 <prabhutiwari111@gmail.com>
2021-02-21 18:44:19 +0530

    added image to db

--- modal/userSchema.js
+++ modal/userSchema.js
@@ -12,7 +12,22 @@ const userSchema = mongoose.Schema({
      },
      password: {
          type: String, 
+     },
+     img: {
+         type: String
      }
 })
 
+
 module.exports = mongoose.model('User', userSchema);
+
+// const imageSchema = new mongoose.Schema({
+//     name: String,
+//     desc: String,
+//     img:
+//     {
+//         data: Buffer,
+//         contentType: String
+//     }
+// });
+// module.exports = mongoose.model('ImageSchema', imageSchema);
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,34 @@ const router = express.Router();
 const User = require('../modal/userSchema')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const fs = require('fs');
+var path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null,'./uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+const upload = multer({storage: storage, limits: {
+  fileSize: 1024 * 1024 *2
+}});
 
 /* GET users listing.*/
-router.post('/createuser', function (req, res, next) {
+router.post('/createuser', upload.single('userImage') ,function (req, res, next) {
+  console.log("line13", req.file);
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
     // Now we can store the password hash in db.
     var user = new User({
       name: req.body.name,
       email: req.body.email,
       phone: req.body.phone,
-      password: hash
+      password: hash,
+      img: req.file.path
     });
     // Save the new model instance, passing a callback
     user.save(function (err) {
